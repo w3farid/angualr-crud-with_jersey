8277ec497d7fe8d17a22b76b29ffa872ce45bdab
Your Name <you@example.com>
2020-11-16 13:36:31 +0600

    update add layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LayoutComponent } from './layout/layout.component';
 import { ProductComponent } from './product/product.component';
+import { StudentComponent } from './student/student.component';
 
 const routes: Routes = [
+  
   {
-    path: 'prodcut', component: ProductComponent
-  }
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'signup', component: SignupComponent
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'student',
+        component: StudentComponent
+      },
+      {
+        path: 'product', component: ProductComponent
+      },
+    ]
+    
+  },
+  {
+    path: '**', redirectTo: 'dashboard'
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentComponent } from './student/student.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    LayoutComponent,
+    DashboardComponent,
+    StudentComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
